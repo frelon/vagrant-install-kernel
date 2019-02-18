e6c97d682a04d7396dd319b41432ec6e8995f753
Fredrik Lonnegren <fredrik.lonnegren@dgc.se>
2019-02-18 18:37:42 +0100

    Move to 5.0-rc7

--- install-linux-kernel.sh
+++ install-linux-kernel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-KERNEL_VERSION=5.0-rc3
+KERNEL_VERSION=5.0-rc7
 echo "Going to install kernel version ${KERNEL_VERSION}"
 
 if [ ! -f /tmp/kernels/linux-${KERNEL_VERSION}.tar.gz ]; then
